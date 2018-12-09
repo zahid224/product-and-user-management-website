716aaf9a7e740804a29682fa904a9292c16e022a
Zahid Mahmood <45736576+zahid224@users.noreply.github.com>
2018-12-09 10:14:07 -0800

    Add files via upload

---
+++ ManageUser.php
@@ -0,0 +1,22 @@
+<?php
+include 'nave.php';
+require 'AdminTest.php';
+
+?>
+<div align="center">
+    <table class="tb">
+        <tr>
+   <td> <button class="button" id="ViewUsers">View Users</button></td>
+    <td>  <button class="button" id="addUserButton">Add Users</button></td>
+        </tr>
+    </table>
+</div>
+<div id="UserForm"></div>
+<table id="Userdata" border="2px" cellspacing="5px" align="center" Style="background-color:whitesmoke; padding:5px;" ></table>
+<?php
+include 'footer.php';
+?>
+
+</body>
+</html>
+
